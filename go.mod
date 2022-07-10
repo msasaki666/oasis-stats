8f35a6b4748cf470443c13093a095a33b88ee303
Sasaki-Motoaki <voyegesansretour1@gmail.com>
2022-07-10 17:51:04 +0900

    雑に利用状況のテキストを抽出する処理を作成

--- go.mod
+++ go.mod
@@ -3,7 +3,13 @@ module github.com/msasaki666/oasis-stats
 go 1.18
 
 require (
-	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/PuerkitoBio/goquery v1.8.0
+	github.com/otiai10/gosseract v2.2.1+incompatible
+)
+
+require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/otiai10/mint v1.3.2 // indirect
+	github.com/pkg/errors v0.9.1
 	golang.org/x/net v0.0.0-20220708220712-1185a9018129 // indirect
 )
--- main.go
+++ main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/otiai10/gosseract"
+	"github.com/pkg/errors"
 )
 
-func ExampleScrape() {
+func scrapeOasisUsageStats() {
+	client := gosseract.NewClient()
+	defer client.Close()
+
 	// Request the HTML page.
-	res, err := http.Get("http://metalsucks.net")
+	res, err := http.Get("https://www.sportsoasis.co.jp/sh07/usage_stats/")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(errors.WithStack(err).Error())
 	}
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
@@ -22,17 +28,39 @@ func ExampleScrape() {
 	// Load the HTML document
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(errors.WithStack(err).Error())
 	}
 
 	// Find the review items
-	doc.Find(".left-content article .post-title").Each(func(i int, s *goquery.Selection) {
+	doc.Find("#point2 ul").Each(func(i int, s *goquery.Selection) {
 		// For each item found, get the title
-		title := s.Find("a").Text()
-		fmt.Printf("Review %d: %s\n", i, title)
+		url, exists := s.Find("li div img").First().Attr("src")
+		if !exists {
+			log.Fatal("利用状況を表示する要素が見つからん")
+		}
+		res, err := http.Get(url)
+		if err != nil {
+			log.Fatal(errors.WithStack(err).Error())
+		}
+		defer res.Body.Close()
+		if res.StatusCode != 200 {
+			log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+		}
+		b, err := io.ReadAll(res.Body)
+		if err != nil {
+			log.Fatal(errors.WithStack(err).Error())
+		}
+		if err = client.SetImageFromBytes(b); err != nil {
+			log.Fatal(errors.WithStack(err).Error())
+		}
+		t, err := client.Text()
+		if err != nil {
+			log.Fatal(errors.WithStack(err).Error())
+		}
+		fmt.Printf("%s\n", t)
 	})
 }
 
 func main() {
-	ExampleScrape()
+	scrapeOasisUsageStats()
 }
